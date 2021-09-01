c7d3d44bc7c4b44d6713b5f04e09e2dd0769c45b
Ada <interimgoliath@gmail.com>
2021-09-01 05:35:04 +0000

    Fix this

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -1,10 +1,10 @@
 const fs = require("fs");
 const { REST } = require("@discordjs/rest");
 const { Routes } = require("discord-api-types/v9");
-const { client_id, guild_id, token } = require("../config.json");
+const { client_id, guild_id, token } = require("./config.json");
 
 const commands = [];
-const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
+const commandFiles = fs.readdirSync(`${__dirname}/commands`).filter(file => file.endsWith(".js"));
 
 for (const file of commandFiles) {
   const command = require(`./commands/${file}`);
