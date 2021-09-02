145f61b72047aa51c5409818ecfc6dff7871ebc4
Ada <interimgoliath@gmail.com>
2021-09-02 17:36:12 +0000

    promises

--- src/index.js
+++ src/index.js
@@ -1,35 +1,44 @@
-import { readdirSync } from "fs";
+import { readdir } from "fs/promises";
 import { execSync } from "child_process";
 import { Client, Collection, Intents } from "discord.js";
 import { token } from "./config.js";
 import logger from "./logging.js";
 import deploy_commands from "./deploy-commands.js";
 
-logger.info("Deploying guild commands...");
-deploy_commands();
-
-logger.info("Setting up the client...");
+logger.info("Starting...");
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 client.revision = execSync("git rev-parse HEAD").toString().trim();
-
-const
-  commandFiles = readdirSync(new URL("commands", import.meta.url)).filter(f => f.endsWith(".js")),
-  eventFiles = readdirSync(new URL("events", import.meta.url)).filter(f => f.endsWith(".js"));
-
 client.commands = new Collection();
-for (const file of commandFiles) {
-  const command = await import(`./commands/${file}`);
-  client.commands.set(command.data.name, command);
-}
 
-for (const file of eventFiles) {
-  const event = await import(`./events/${file}`);
-  if (event.once) {
-    client.once(event.name, (...args) => event.execute(...args));
-  } else {
-    client.on(event.name, (...args) => event.execute(...args));
-  }
-}
+const setup_tasks = [
+  // Register commands on discord's side
+  deploy_commands(),
 
-client.login(token);
+  // Commands
+  readdir(new URL("commands", import.meta.url))
+    .then(files => files
+      .filter(f => f.endsWith(".js"))
+      .forEach(async f => {
+        const command = await import(`./commands/${f}`);
+        client.commands.set(command.data.name, command);
+      }))
+    .then(() => logger.info("Loaded commands.")),
+
+  // Events
+  readdir(new URL("events", import.meta.url))
+  .then(files => files
+    .filter(f => f.endsWith(".js"))
+    .forEach(async f => {
+      const event = await import(`./events/${f}`);
+      if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+      } else {
+        client.on(event.name, (...args) => event.execute(...args));
+      }
+    }))
+  .then(() => logger.info("Loaded events."))
+];
+
+Promise.all(setup_tasks)
+  .then(() => client.login(token));
